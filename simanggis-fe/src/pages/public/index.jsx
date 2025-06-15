1c1143dbb3e6c960c8521b86adf895956edb064c
AdrianFakhriza <adrianfakhriza0@gmail.com>
2025-06-15 13:54:26 +0700

    add statistic

--- simanggis-fe/src/pages/public/index.jsx
+++ simanggis-fe/src/pages/public/index.jsx
@@ -1,12 +1,12 @@
 import Hero from "../../components/hero";
-// import Statistic from "../../components/statistic";
+import Statistic from "../../components/statistic";
 import Testimonial from "../../components/testimonial";
 
 export default function Home() {
     return (
       <>
         <Hero />
-        {/* <Statistic /> */}
+        <Statistic />
         <Testimonial />
       </>
     );
