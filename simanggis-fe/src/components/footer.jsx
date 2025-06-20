182c26bca4a8357c141e7d847455e0e24c3e5937
Najlamhr <maharaninajla37@gmail.coom>
2025-06-20 19:19:30 +0700

    perbaiki error footer

--- simanggis-fe/src/components/footer.jsx
+++ simanggis-fe/src/components/footer.jsx
@@ -83,7 +83,7 @@ export default function Footer() {
           </div>
         </div>
 
-        {/* Copyright */}
+
         <div className="mt-10 border-t border-purple-200 dark:border-purple-800 pt-6 text-sm text-gray-500 dark:text-gray-400 text-center">
           © 2025{" "}
           <span className="font-semibold text-purple-600 dark:text-purple-400">
@@ -93,57 +93,5 @@ export default function Footer() {
         </div>
       </div>
     </footer>
-=======
-    <>
-      <footer className="border-t-2 p-4 bg-white md:p-8 lg:p-10 dark:bg-gray-800">
-        <div className="mx-auto max-w-screen-xl text-center">
-          <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
-            <li>
-              <Link to={"#"} className="mr-4 hover:underline md:mr-6 ">
-                About
-              </Link>
-            </li>
-            <li>
-              <Link to={"#"} className="mr-4 hover:underline md:mr-6">
-                Premium
-              </Link>
-            </li>
-            <li>
-              <Link to={"#"} className="mr-4 hover:underline md:mr-6 ">
-                Campaigns
-              </Link>
-            </li>
-            <li>
-              <Link to={"#"} className="mr-4 hover:underline md:mr-6">
-                Blog
-              </Link>
-            </li>
-            <li>
-              <Link to={"#"} className="mr-4 hover:underline md:mr-6">
-                Affiliate Program
-              </Link>
-            </li>
-            <li>
-              <Link to={"#"} className="mr-4 hover:underline md:mr-6">
-                FAQs
-              </Link>
-            </li>
-            <li>
-              <Link to={"#"} className="mr-4 hover:underline md:mr-6">
-                Contact
-              </Link>
-            </li>
-          </ul>
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            ©{" "}
-            <Link to={"#"} className="hover:underline">
-              2025
-            </Link>
-            . All Rights Reserved.
-          </span>
-        </div>
-      </footer>
-    </>
->>>>>>> ffaac75757a68fc2ef76354c5d20166f5a208353
-  );
-}
+  )
+};
--- simanggis-fe/src/main.jsx
+++ simanggis-fe/src/main.jsx
@@ -2,12 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-<<<<<<< HEAD
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
-=======
->>>>>>> ffaac75757a68fc2ef76354c5d20166f5a208353
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
